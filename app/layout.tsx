3e16629e4a94fe8b372fd6a06cffbd49b7fbe7cc
mojavadb <mojavadb@users.noreply.github.com>
2026-05-31 00:18:38 -0700

    add font from google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import "./globals.css";
 
 const vazirmatn = Vazirmatn({
   variable: "--font-vazirmatn",
-  subsets: ["latin"], // ✅ فقط "latin" کافی است (فارسی را هم پوشش می‌دهد)
-  weight: ["400", "500", "700"], // اختیاری: وزن‌های مورد نیاز
+  subsets: ["latin"], 
+  weight: ["400", "500", "700"],
+  display: "swap",
 });
 
 export default function RootLayout({
