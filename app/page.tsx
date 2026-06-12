80cc63b5e3cc3389047a998ee2694a1fcb9ac89f
mojavadb <mojavadb@users.noreply.github.com>
2026-06-12 08:10:05 -0700

    add prisma ORM to project based on Postgresql

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Main from "@/components/Main";
 import { linksTable } from "@/lib/db/links";
+import prisma from "@/lib/prisma";
 
 interface LinkItem {
   id: string;
@@ -9,10 +10,12 @@ interface LinkItem {
   expiresAt: number;
 }
 
-function Home() {
+async function Home() {
   // چون اینجا سرور کامپوننته میتونیم داده رو مستقیما از پایگاه داده بیاریم
   // نیازی به بک اند نیست
   const data : LinkItem[] = linksTable.all();
+  const links = await prisma.link.findMany();
+  console.log(links.length);
   return(
     <Main data={data} />
   );
