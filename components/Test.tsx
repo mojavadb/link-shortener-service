debad87b245bad1975ecfc5896d439562d0794da
mojavadb <mojavadb@users.noreply.github.com>
2026-06-13 08:25:56 -0700

    remove /a route (added for test)

--- components/Test.tsx
+++
@@ -1,8 +0,0 @@
-import { LinkItem } from "@/app/generated/prisma/client";
-
-export default function Test({data} : {data : LinkItem[]}) {
-    
-    return <div>
-            {data?.map(item => <div key={item.id}>{item.id}</div>)}
-    </div>;
-}
