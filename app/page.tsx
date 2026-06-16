ef1219eb91b35a8788857771d82edf97b399496d
mojavadb <mojavadb@users.noreply.github.com>
2026-06-15 20:49:58 -0700

    add User model to schema.prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Main from "@/components/Main";
 import prisma from "@/lib/prisma";
-import { LinkItem } from "./generated/prisma/client";
+import { LinkItem, User } from "./generated/prisma/client";
 
 async function Home() {
   // چون اینجا سرور کامپوننته میتونیم داده رو مستقیما از پایگاه داده بیاریم
   // نیازی به بک اند نیست
   const links : LinkItem[] = await prisma.linkItem.findMany();
-  console.log(links.length);
+  const users : User[] = await prisma.user.findMany();
+  console.log(users);
   return(
     <Main data={links} />
   );
