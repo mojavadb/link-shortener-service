37740a94b17785266a1513db13c5e4eb530e19fd
mojavadb <mojavadb@users.noreply.github.com>
2026-06-17 00:24:12 -0700

    some comments

--- auth.ts
+++ auth.ts
@@ -19,4 +19,22 @@ session همه چیز مربوط به کاربر حذف میشود
 
 دسترس کاربران کنترل شده و امن تر است: به دلیل ذخیره
 در پایگاه داده
+
+چون که از پریسما استفاده میکنیم باید آداپترش را
+هم نصب کنیم
+*/
+
+/*
+جریان کاری لاگین:
+google => auth js => DB (User + session) => cookie
+در هر درخواست داریم:
+cookie => session lookup in DB => User => access granted
+لاگ اوت:
+session deleted from Db => cookie cleared
+*/
+
+/* 
+نقش auth js:
+login - session creation - session validation - logout
+cookie management
 */
