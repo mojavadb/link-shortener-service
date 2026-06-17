8e066cee7e8772b46512f28e7b88a2802f4a6c7c
mojavadb <mojavadb@users.noreply.github.com>
2026-06-17 00:21:53 -0700

    some comments

--- auth.ts
+++ auth.ts
@@ -5,3 +5,18 @@ import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
 })
+
+
+/*
+احراز هویت از روش session based (database session):
+چون که:
+
+این سیستم با محوریت کاربر است، هر کاربر یک یا
+چند لینک میتواند بسازد. باید همه چیز ذخیره شود.
+
+ورود و خروج کاربر ها کاملا کنترل میشود: با حذف
+session همه چیز مربوط به کاربر حذف میشود
+
+دسترس کاربران کنترل شده و امن تر است: به دلیل ذخیره
+در پایگاه داده
+*/
