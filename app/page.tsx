3bc09ad731885d50e9e6792b60da8e5081025a55
mojavadb <mojavadb@users.noreply.github.com>
2026-06-22 23:00:13 -0700

    remove data from /Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Main from "@/components/Main";
 import prisma from "@/lib/prisma";
 import { Account, LinkItem, User, Session } from "./generated/prisma/client";
-import { auth, signIn, signOut } from "@/auth";
-import { redirect } from "next/navigation";
 
 async function Home() {
   // چون اینجا سرور کامپوننته میتونیم داده رو مستقیما از پایگاه داده بیاریم
@@ -11,9 +9,12 @@ async function Home() {
   const users: User[] = await prisma.user.findMany();
   const accounts: Account[] = await prisma.account.findMany();
   const sessions: Session[] = await prisma.session.findMany();
-  console.log(links.length, users.length, accounts.length, sessions.length);
-  console.log(sessions);
-  // if (!session?.user) redirect("/auth");
+  console.log(
+    links.length,
+    users.length,
+    accounts.length,
+    sessions.length
+  );
   return (
       <Main />
   );
